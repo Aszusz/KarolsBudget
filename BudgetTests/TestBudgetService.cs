b6630ec81fdef471ae29ea39e31f14896e7db167
Adrian Szuszkiewicz <adrianszuszkiewicz@gmail.com>
2019-02-13 01:35:04 +0100

    Add day by day tests

--- BudgetTests/TestBudgetService.cs
+++ BudgetTests/TestBudgetService.cs
@@ -52,11 +52,79 @@ namespace BudgetTests
             var today = new DateTime(2019, 02, 2);
             _dateProvider.SetToday(today);
 
-            _budget.AddExpense("Wydatek-2A", today, 50);
+            _budget.AddExpense("Wydatek-1A", new DateTime(2019, 02, 1), 30);
+            _budget.AddExpense("Wydatek-1B", new DateTime(2019, 02, 1), 40);
+            _budget.AddExpense("Wydatek-2A", today, 145);
 
             Assert.That(_budgetService.CalculateSavings(_budget), Is.EqualTo(30));
             Assert.That(_budgetService.CalculateTodaysAllowance(_budget), Is.EqualTo(100));
             Assert.That(_budgetService.CalculateRemainingBudget(_budget), Is.EqualTo(300));
+        }
+
+        [Test]
+        public void TestDay3()
+        {
+            var today = new DateTime(2019, 02, 3);
+            _dateProvider.SetToday(today);
+
+            _budget.AddExpense("Wydatek-1A", new DateTime(2019, 02, 1), 30);
+            _budget.AddExpense("Wydatek-1B", new DateTime(2019, 02, 1), 40);
+            _budget.AddExpense("Wydatek-2A", new DateTime(2019, 02, 2), 145);
+            _budget.AddExpense("Wydatek-3A", today, 30);
+
+            Assert.That(_budgetService.CalculateSavings(_budget), Is.EqualTo(0));
+            Assert.That(_budgetService.CalculateTodaysAllowance(_budget), Is.EqualTo(95));
+            Assert.That(_budgetService.CalculateRemainingBudget(_budget), Is.EqualTo(190));
+        }
+
+        [Test]
+        public void TestDay4()
+        {
+            var today = new DateTime(2019, 02, 4);
+            _dateProvider.SetToday(today);
+
+            _budget.AddExpense("Wydatek-1A", new DateTime(2019, 02, 1), 30);
+            _budget.AddExpense("Wydatek-1B", new DateTime(2019, 02, 1), 40);
+            _budget.AddExpense("Wydatek-2A", new DateTime(2019, 02, 2), 145);
+            _budget.AddExpense("Wydatek-3A", new DateTime(2019, 02, 2), 30);
+
+            Assert.That(_budgetService.CalculateSavings(_budget), Is.EqualTo(55));
+            Assert.That(_budgetService.CalculateTodaysAllowance(_budget), Is.EqualTo(100));
+            Assert.That(_budgetService.CalculateRemainingBudget(_budget), Is.EqualTo(100));
+        }
+
+        [Test]
+        public void TestDay5()
+        {
+            var today = new DateTime(2019, 02, 5);
+            _dateProvider.SetToday(today);
+
+            _budget.AddExpense("Wydatek-1A", new DateTime(2019, 02, 1), 30);
+            _budget.AddExpense("Wydatek-1B", new DateTime(2019, 02, 1), 40);
+            _budget.AddExpense("Wydatek-2A", new DateTime(2019, 02, 2), 145);
+            _budget.AddExpense("Wydatek-3A", new DateTime(2019, 02, 2), 30);
+            _budget.AddExpense("Wydatek-5A", today, 27);
+
+            Assert.That(_budgetService.CalculateSavings(_budget), Is.EqualTo(155));
+            Assert.That(_budgetService.CalculateTodaysAllowance(_budget), Is.EqualTo(100));
+            Assert.That(_budgetService.CalculateRemainingBudget(_budget), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void TestPost()
+        {
+            var today = new DateTime(2019, 02, 6);
+            _dateProvider.SetToday(today);
+
+            _budget.AddExpense("Wydatek-1A", new DateTime(2019, 02, 1), 30);
+            _budget.AddExpense("Wydatek-1B", new DateTime(2019, 02, 1), 40);
+            _budget.AddExpense("Wydatek-2A", new DateTime(2019, 02, 2), 145);
+            _budget.AddExpense("Wydatek-3A", new DateTime(2019, 02, 2), 30);
+            _budget.AddExpense("Wydatek-5A", new DateTime(2019, 02, 5), 27);
+
+            Assert.That(_budgetService.CalculateSavings(_budget), Is.EqualTo(228));
+            Assert.That(_budgetService.CalculateTodaysAllowance(_budget), Is.EqualTo(0));
+            Assert.That(_budgetService.CalculateRemainingBudget(_budget), Is.EqualTo(0));
         }
     }
 }
